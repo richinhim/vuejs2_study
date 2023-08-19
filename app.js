6e5daffa1d10f7f1d271e7a8a659cc4dd90d830c
sooyon lee <kwlsy666@naver.com>
2023-08-19 09:36:44 +0900

    Vue JS 2 Tutorial #3 - Data & Methods

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ new Vue({
   el: "#vue-app",
   data: {
     name: "Shaun",
+    job: "Ninja",
+  },
+  methods: {
+    greet: function (time) {
+      return "Good " + time + " " + this.name;
+    },
   },
 });
