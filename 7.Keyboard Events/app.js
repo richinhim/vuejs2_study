9c3c0825225df436930c21b8d988d12c27a202dd
sooyon lee <kwlsy666@naver.com>
2023-08-19 14:09:26 +0900

    Vue JS 2 Tutorial #7 - Keyboard Events

---
+++ 7.Keyboard Events/app.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: "#vue-app",
+  data: {},
+  methods: {
+    logName: function () {
+      console.log("you entered your name");
+    },
+
+    logAge: function () {
+      console.log("you entered your age");
+    },
+  },
+});
